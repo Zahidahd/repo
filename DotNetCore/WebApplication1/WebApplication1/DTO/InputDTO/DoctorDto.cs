9620a3ac18ad0a3f5351f0d06f3958ba32017d91
Allahghafoor <122606313+Allahghafoor@users.noreply.github.com>
2023-01-25 23:10:59 +0530

    DoctorDto committed

---
+++ DotNetCore/WebApplication1/WebApplication1/DTO/InputDTO/DoctorDto.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.DTO.InputDTO
+{
+    public class DoctorDto
+    {
+
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string  Department  { get; set; }
+
+        public string City { get; set; }
+    }
+}
