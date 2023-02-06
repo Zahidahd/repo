9d52d8ccd5a48efba791e9dda1a4d831ba1dde11
zahidahd <zahid.ahd12@gmail.com>
2023-02-06 22:53:21 +0530

    Sql Tables.

---
+++ DotNetCore/Database/Tables/Teachers_DDL.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Teachers
+(
+Id INT IDENTITY(1,1),
+FullName NVARCHAR(50) NOT NULL,
+Email NVARCHAR(50) NOT NULL,
+Age INT,
+Gender INT,
+SchoolName NVARCHAR(50),
+Department NVARCHAR(50),
+Salary INT
+CONSTRAINT UC_Teachers UNIQUE(Email)
+)
+
+SELECT * FROM Teachers
