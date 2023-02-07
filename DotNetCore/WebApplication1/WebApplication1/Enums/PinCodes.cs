c75d3775606b85ae64f3bca342e2ef48898af30d
zahidahd <zahid.ahd12@gmail.com>
2023-02-07 10:08:36 +0530

    Sql file name changed

---
+++ DotNetCore/WebApplication1/WebApplication1/Enums/PinCodes.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1.Enums
+{
+    public enum PinCodes
+    {
+        Vaishali = 843104,
+        Muzaffarpur = 843104,
+        Samastipur = 848130,
+        Kukatpally = 500072,
+        Thane = 400601,
+        Noida = 201301,
+        Chennai = 600001,
+        AndheriEast = 400069,
+        Ghaziabad = 201001,
+        Hyderabad = 500001,
+        Ameerpet = 509325,
+        Bangalore = 560001,
+        Guntur = 522002,
+        Mumbai = 400001,
+        Whitefield = 560066
+    }
+}
