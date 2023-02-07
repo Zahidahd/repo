6e230b128d27a58ddb4af157c325005df6d86ae9
zahidahd <zahid.ahd12@gmail.com>
2023-02-07 20:05:25 +0530

    Sql Indenting issue resolved.

--- DotNetCore/Database/Tables/dbo.Customers.sql
+++ DotNetCore/Database/Tables/dbo.Customers.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Customers
 (
-Id INT IDENTITY(1,1),
-Name NVARCHAR(50),
-Email NVarchar(50),
-Gender VARCHAR(10),
-Age INT,
-Country NVARCHAR(50),
-CONSTRAINT UC_Customers UNIQUE(Email)
+	Id INT IDENTITY(1,1),
+	Name NVARCHAR(50),
+	Email NVarchar(50),
+	Gender VARCHAR(10),
+	Age INT,
+	Country NVARCHAR(50),
+	CONSTRAINT UC_Customers UNIQUE(Email)
 )
